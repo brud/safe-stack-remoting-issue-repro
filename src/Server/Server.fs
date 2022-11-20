dfb15ec2b8123545f25aff6efea190fcb2e88d01
Anton <brud.90@gmail.com>
2022-11-20 06:03:11 +0200

    simplify model

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -5,62 +5,18 @@ open Fable.Remoting.Server
 open Fable.Remoting.Giraffe
 open Microsoft.AspNetCore.Http
 open Saturn
-
 open Shared
-
 
 let todosApi =
     {
-      getUser =
-          fun() -> async {
-              return {
-                Id = "99c19cba-dbce-4359-8be2-edb1892db452"
-                UserName = "test@redx.su"
-                Sites =
-                [
-                    {
-                        Id = 2
-                        DomainName = "test.com"
-                        Pages = [
-                            {
-                                Id = 2
-                                SiteId = 2
-                                Uri = "/"
-                                Title = "bla"
-                                Description = ""
-                                Keywords = seq []
-                                Contents = [
-                                    {
-                                        Id = 1270
-                                        Position = 0
-                                        Type = Tag
-                                        PageId = Some 2
-                                        Children = []
-                                        ParentId = None
-                                        Value = None
-                                        Name = "html"
-                                        CssId = None
-                                        CssClass = None
-                                        IsTemplate = false
-                                        IsEditable = false
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ]
-                }
-          }
+      getUser = fun() -> async { return { Type = Tag } }
     }
+
 let errorHandler (ex: Exception) (routeInfo: RouteInfo<HttpContext>) =
-    match ex with
-    | :? Exception as ex ->
-        let customError = {
-            error = ex.ToString()
-            ignored = false
-            handled = true }
-        Propagate customError
-    | _ -> Ignore // ignore error
+    Propagate { error = ex.ToString()
+                ignored = false
+                handled = true }
+
 let webApp =
     Remoting.createApi ()
     |> Remoting.withErrorHandler errorHandler
--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -1,39 +1,10 @@
 namespace Shared
 
-type ContentType' = Tag | Attribute | Text
+
+type UserType' = Tag | Member | Admin
 
 type User' = {
-    Id: string
-    UserName: string
-    Sites: Site' list
-}
-and Site' = {
-    Id: int
-    DomainName: string
-    Pages: Page' list
-}
-and Page' = {
-    Id: int
-    SiteId: int
-    Uri: string
-    Title: string
-    Description: string
-    Keywords: seq<string>
-    Contents: Content' list
-}
-and Content' = {
-    Id: int
-    Position: int
-    Type: ContentType'
-    PageId: int option
-    Children: Content' list
-    ParentId: int option
-    Value: string option
-    Name: string
-    CssId: string option
-    CssClass: string option
-    IsTemplate: bool
-    IsEditable: bool
+    Type: UserType'
 }
 
 module Route =
